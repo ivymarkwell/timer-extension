a8d4a0f4a59c170e11e31b102a4e171e9883f559
ivymarkwell126 <ivy.markwell@utexas.edu>
2018-02-15 23:53:23 -0500

    currently counts in seconds and correctly displays the last time a tab was updated, i.e. new tab, new url

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,21 @@
-window.onload = function(){
-    timeStart = new Date();
-   }
+var elapsedTime = 0;
+var startDate = null;
+ 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.status == 'complete') {
+        startDate = new Date();
+        localStorage.setItem("startDate", startDate);
+    };
+});
 
-setInterval(
-    function(){
-        timeNow = new Date()
-        timeSinceStart = timeNow - timeStart
-        document.getElementById('timeSpent').innerHTML = timeSinceStart;
-    },
-1000)
+window.onload = function(){
+    setTimeout(function() {
+        setInterval(function() {
+            startDate = new Date(localStorage.getItem("startDate"))
+            timeNow = new Date();
+            elapsedTime = timeNow - startDate;
+            elapsedTimeSeconds = Math.round(elapsedTime / 1000)
+            document.getElementById('elapsedTime').innerHTML = elapsedTimeSeconds;
+        }, 100);
+    }, 3);
+};
