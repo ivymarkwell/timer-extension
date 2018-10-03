eda97af83c76c0129788177ed3952cbd2fab793f
ivymarkwell126 <ivy.markwell@utexas.edu>
2018-10-03 01:59:25 -0400

    trying to dynamically set popup text from icon...

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,45 @@
-chrome.tabs.onActivated.addListener(function() {
-    startDate = new Date();
-    localStorage.setItem("startDate", startDate);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    url = changeInfo.url
+
+    if (localStorage.getItem(url) != null) {
+        pageTime = localStorage.getItem(url).split(",");
+    }
+    else {
+        pageTime = null
+    };
 });
 
 function updateTime() {
-    startDate = new Date(localStorage.getItem("startDate"))
     timeNow = new Date();
-    elapsedTime = timeNow - startDate;
-    elapsedTimeSeconds = Math.round(elapsedTime / 1000)
+    startDate = new Date();
+    timeSpent = 0;
+
+    if (typeof(pageTime) != "undefined" && pageTime != null) {
+        startDate = new Date(pageTime[0]);
+        timeSpent = pageTime[1];
+    };
+
+    timeSpent = (timeNow - startDate) + timeSpent
+    elapsedTimeSeconds = Math.round(timeSpent / 1000);
+
+    console.log(timeNow, startDate, timeSpent)
 
     timeElement = document.getElementById("elapsedTime")
     if (typeof(timeElement) != "undefined" && timeElement != null) {
         var date = new Date(null);
-        date.setSeconds(elapsedTimeSeconds);
-        var time = date.toISOString().substr(11, 8);
-        timeElement.innerHTML = time;
-    }
+        date.setSeconds(elapsedTimeSeconds)
+        var time = date.toISOString().substr(11, 8)
+        timeElement.innerHTML = timeSpent
+        alert(timeSpent)
+    };
+
+    if (typeof(url) === "undefined") {
+        url = window.location.hostname
+    };
+
+    // set local storage item
+    pageTime = [startDate, timeSpent]
+    localStorage.setItem(url, pageTime);
 };
 
 setInterval(updateTime, 100);
